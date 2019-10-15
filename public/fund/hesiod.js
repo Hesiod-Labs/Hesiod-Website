cfc2b4344be47aeed392428f7cec78e20e522063
Daniel <danysoares27@gmail.com>
2019-10-15 14:26:22 +0200

    added analysts

--- public/fund/hesiod.js
+++ public/fund/hesiod.js
@@ -1,7 +1,53 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+function initApp() {
+  console.log('init');
+  // Listening for auth state changes.
+  // [START authstatelistener]
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      document.getElementById('body').display = true;
+      document.getElementById('adminTag').display = false;
+      // User is signed in.
+      if (user.email == 'danysoares27@gmail.com') {
+        godMode = true;
+        // document.getElementById('adminTag').display = true;
+        // alert('You are in god mode.');
+      } else {
+        alert('Welcome ' + user.email);
+        // document.getElementById('adminTag').display = false;
+      }
+    } else {
+      // User is signed out.
+      document.getElementById('body').display = false;
+      window.location.pathname = '../index.html';
+    }
+  });
 }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+window.onload = function() {
+  initApp();
+  // drawChart();
+};
+
+
+
+//Chart data
+google.charts.load('current', {packages: ['corechart', 'line']});
+// google.charts.setOnLoadCallback(drawSheetName);
+
+//Spreadsheet
+function drawSheetName() {
+  var query = new google.visualization.Query(
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ64irmLoTxLYYqlfDtizTK4KilcotAta14TapSWITPhJwy17BqSlPL83maYlPbAgSr37rCSElVXFZk/pubhtml?gid=0&single=true');
+  query.send(handleSampleDataQueryResponse);
+}
+
+function handleSampleDataQueryResponse(response) {
+  if (response.isError()) {
+    alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+    return;
+  }
+
+  var data = response.getDataTable();
+  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+  chart.draw(data, { height: 400 });
 }
---
+++ public/fund/login.js
@@ -0,0 +1,56 @@
+function toggleSignIn() {
+  if (firebase.auth().currentUser) {
+    // [START signout]
+    firebase.auth().signOut();
+    // [END signout]
+  } else {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    // Sign in with email and pass.
+    // [START authwithemail]
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      // [END_EXCLUDE]
+    });
+    // [END authwithemail]
+  }
+}
+
+
+function initApp() {
+  console.log('init');
+  // Listening for auth state changes.
+  // [START authstatelistener]
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      // if (user.email == 'danysoares27@gmail.com') {
+      //   alert('Welcome Daniel');
+      // }
+      window.location.pathname = '/fund/hesiod.html';
+    } else {
+      // User is signed out.
+    }
+  });
+}
+
+window.onload = function() {
+  initApp();
+};
